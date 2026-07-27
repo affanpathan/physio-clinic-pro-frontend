ff3d61e542e0082cbead059f17f24a981fa215a1
affanpathan <affan.pathan@gmail.com>
2026-07-27 14:10:31 +0530

    debug

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -22,6 +22,7 @@ export default function Dashboard({ navigate }) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
+console.log('${API_URL}/dashboard');
   useEffect(() => {
     fetch('${API_URL}/dashboard')
       .then(r => r.json())
